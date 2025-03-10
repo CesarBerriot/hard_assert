611531df35f97a4e89703a2af938fce030180a9e
César Berriot <berriot.cesar@gmail.com>
2025-03-10 20:07:56 +0100

    setup_gtest integration fix

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,11 +1,11 @@
 include(FetchContent)
 
 FetchContent_Declare(
-  SetupGTest
-  GIT_REPOSITORY https://github.com/CesarBerriot/SetupGTest.git
+  setup_gtest
+  GIT_REPOSITORY https://github.com/CesarBerriot/setup_gtest.git
   GIT_TAG 1.0.0
 )
 
-FetchContent_MakeAvailable(SetupGTest)
+FetchContent_MakeAvailable(setup_gtest)
 
-include(SetupGTest)
+include(${setup_gtest_SOURCE_DIR}/setup_gtest.cmake)
